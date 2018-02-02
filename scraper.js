4d058841cf11afa25befa7d0076d2ffda3033b0b
Caroline Forslund <cf222ev@student.lnu.se>
2018-02-02 09:20:30 +0100

    Creates data directory if it doesn't exist

--- scraper.js
+++ scraper.js
@@ -1,23 +1,19 @@
-//Require the fs module (which provides an API) to be able to interact with the file system
-const fs = require('fs');
+const fs = require('fs'); //Require the fs module (which provides an API) to be able to interact with the file system
+const path = './data';    //Path to data folder
 
-if (!fs.existsSync('./data')) {
-    console.log('no data folder found');
+//Check if data folder exists
+//If not, create data folder
+if (!fs.existsSync(path)) {
+  console.log('no data folder found');
+  fs.mkdir(path, (error) => {
+    if(error){
+      console.log('failed to create directory', error);
+    }
+    else {
+      console.log('successfully created directory');
+    }
+  });
 }
 else {
   console.log('data folder found');
 }
-
-// //function will check if a directory exists, and create it if it doesn't
-// function checkDirectory(directory, callback) {
-//   fs.stat(directory, function(err, stats) {
-//     //Check if error defined and the error code is "not exists"
-//     if (err && err.errno === 34) {
-//       //Create the directory, call the callback.
-//       fs.mkdir(directory, callback);
-//     } else {
-//       //just in case there was a different error:
-//       callback(err)
-//     }
-//   });
-// }
