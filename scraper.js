2552520c7f1007892b246ebf299963e756aeaf2b
Caroline Forslund <cf222ev@student.lnu.se>
2018-02-02 09:40:57 +0100

    scrape-it package added to project

--- scraper.js
+++ scraper.js
@@ -1,8 +1,7 @@
 const fs = require('fs'); //Require the fs module (which provides an API) to be able to interact with the file system
 const path = './data';    //Path to data folder
 
-//Check if data folder exists
-//If not, create data folder
+// Check if data folder exists. If not, create data folder =====================
 if (!fs.existsSync(path)) {
   console.log('no data folder found');
   fs.mkdir(path, (error) => {
