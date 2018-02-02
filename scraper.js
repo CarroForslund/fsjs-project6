8f1882a2d77e0bcc6faca8a0e58b1592dfa1a3ae
Caroline Forslund <cf222ev@student.lnu.se>
2018-02-02 12:06:06 +0100

    Can get detailed product data

--- scraper.js
+++ scraper.js
@@ -1,10 +1,13 @@
-const fs = require('fs'); //Require the fs module (which provides an API) to be able to interact with the file system
-const path = './data';    //Path to data folder
+const fs = require('fs');                     //Require the fs module (which provides an API) to be able to interact with the file system
+const scrapeIt = require("scrape-it");        //Require Scrape It to be able to scrape website for t-shirt information
+const dataDirPath = './data';                 //Path to data folder
+const sitePath = 'http://shirts4mike.com/';//Path to website to scrape
+
 
 // Check if data folder exists. If not, create data folder =====================
-if (!fs.existsSync(path)) {
+if (!fs.existsSync(dataDirPath)) {
   console.log('no data folder found');
-  fs.mkdir(path, (error) => {
+  fs.mkdir(dataDirPath, (error) => {
     if(error){
       console.log('failed to create directory', error);
     }
@@ -16,3 +19,63 @@ if (!fs.existsSync(path)) {
 else {
   console.log('data folder found');
 }
+
+// The scraper gets the price, title, url and image url from the product page
+// Callback interface: scrapeIt( url, options, callback )
+scrapeIt(sitePath, {
+  // Fetch the list items on the start page
+  pages: {
+    listItem: 'ul.products li',
+    data: {
+      url: {
+        selector: 'a',
+        attr: 'href'
+      }
+    }
+  }
+}, (err, { data }) => {
+  // const errorMessage = `There’s been a 404 error. Cannot connect to ${sitePath}`;
+  // console.log(err || data);
+  if(err){
+    console.error(`There’s been a 404 error. Cannot connect to ${sitePath}`);
+  }
+  else {
+    // console.log(data);
+    const pages = data.pages;
+
+    for (const page of pages){
+      const pagePath = sitePath + page.url;
+      console.log(pagePath);
+
+      // Fetch the list items on the specific page
+      scrapeIt(pagePath, {
+
+        page: {
+          listItem: '.page',
+          data: {
+            price: 'span',
+            title: {
+              selector: 'img',
+              attr: 'alt',
+            },
+            img: {
+              selector: 'img',
+              attr: 'src'
+            }
+          }
+        }
+      }, (err, {data}) => {
+        // console.log(err || data.page)
+        if(err){
+          console.error('An error occured', err);
+        }
+        else {
+          console.log('Price: ', data.page[0].price);
+          console.log('Title: ', data.page[0].title);
+          console.log('Url: ', pagePath);
+          console.log('Img url: ', data.page[0].img);
+        }
+      })
+    }
+  }
+})
